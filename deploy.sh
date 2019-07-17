b37e8c2a60a03d4ede3596bd7fda1f06872db23b
Justin Lundy <justin.lundy@deutschinc.com>
2019-07-17 12:25:17 -0700

    Adding deploy steps to travis

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+
+$(aws ecr get-login --no-include-email --region us-west-2)
+
+docker build -t julundy/multi-client:latest -t 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-client:latest -t julundy/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t julundy/multi-server:latest -t 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-server:latest-t julundy/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t julundy/multi-worker:latest -t 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-worker:latest-t julundy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-client:latest
+docker push 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-server:latest
+docker push 894511344406.dkr.ecr.us-west-2.amazonaws.com/julundy/multi-worker:latest
+docker run -e CI=true julundy/multi-docker npm test
+# docker push julundy/multi-client:latest
+# docker push julundy/multi-server:latest
+# docker push julundy/multi-worker:latest
+
+# docker push julundy/multi-client:$SHA
+# docker push julundy/multi-server:$SHA
+# docker push julundy/multi-worker:$SHA
+
+# kubectl apply -f k8s
+# kubectl set image deployments/server-deployments server=julundy/multi-server:$SHA
+# kubectl set image deployments/client-deployments client=julundy/multi-client:$SHA
+# kubectl set image deployments/worker-deployments worker=julundy/multi-worker:$SHA
---
+++ setup.sh
@@ -0,0 +1,2 @@
+$ pip3 install awscli --upgrade --user
+$ docker build -t julundy/multi-docker -f ./client/Dockerfile.dev ./client
